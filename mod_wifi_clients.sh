2f651e72364706576f3d148f4de5d2b65d8919e9
Stefan Knaak <stefan@knaak.org>
2021-09-17 04:23:15 +0200

    Update mod_wifi_clients.sh
    fix WiFi Clients

--- mod_wifi_clients.sh
+++ mod_wifi_clients.sh
@@ -7,6 +7,7 @@ readonly SCRIPT_debug=$1
 readonly DHCP_HOSTNAMESMAC="/opt/tmp/dhcp_clients_mac.txt"
 readonly CLIENTLIST="/opt/tmp/client-list.txt"
 readonly DATA_TEMP_FILE="/opt/tmp/$SCRIPT_NAME.influx"
+readonly DHCP_EXTERNAL="/opt/tmp/dhcp_external.csv"
 
 #generate new clientlist
 $SCRIPT_DIR/helper_dhcpstaticlist.sh >/dev/null 2>&1
@@ -61,58 +62,65 @@ mod_wifi_clients()
     #printf "$wlan \n"
 
         for client in $clients; do
-            client_lc=$(lc "$client")
-            ip=$(grep $client_lc /proc/net/arp | awk '{print $1}')
 
-            if [ "$ip" = "" ]; then
-              ip=$client
+            if [ ! -d $DHCP_EXTERNAL ]; then
+              STATION_NAME=$(grep -i $client $DHCP_EXTERNAL | cut -d ';' -f3 )
+              ip=$(grep -i $client $DHCP_EXTERNAL | cut -d ';' -f2 )
+            else
+              client_lc=$(lc "$client")
+              ip=$(grep $client_lc /proc/net/arp | awk '{print $1}')
+
+              if [ "$ip" = "" ]; then
+                ip=$client
+              fi
+              #host=$(grep -i $client_lc /mnt/routerUSB/scripts/clients.txt | awk '{print $1}')
+              STATION_NAME=$(grep -i $client_lc $DHCP_HOSTNAMESMAC | awk '{print $1}')                       
+
+              if [ "$STATION_NAME" = "" ]; then
+                  STATION_NAME=$(grep -i $client_lc $CLIENTLIST | head -1 | awk '{print $1}')                
+                  if [ "$STATION_NAME" = "" ]; then
+                    STATION_NAME=$client
+                  fi
+              fi
             fi
-            #host=$(grep -i $client_lc /mnt/routerUSB/scripts/clients.txt | awk '{print $1}')
-            STATION_NAME=$(grep -i $client_lc $DHCP_HOSTNAMESMAC | awk '{print $1}')                       
 
-            if [ "$STATION_NAME" = "" ]; then
-                STATION_NAME=$(grep -i $client_lc $CLIENTLIST | head -1 | awk '{print $1}')                
-                if [ "$STATION_NAME" = "" ]; then
-                  STATION_NAME=$client
-                fi
-            fi
+            wl_info=$(wl -i $wlan sta_info $client)            
 
-            #printf "$wlan \t "$client_lc" \t "$ip" \t "$STATION_NAME" \\n"
-            #debug all wifi infos
-            #wl -i $wlan sta_info $client 
+            rsi_antenna1=$(echo "$wl_info" | awk '/per antenna rssi of last rx data frame/ {print $9}')
+            rsi_antenna2=$(echo "$wl_info" | awk '/per antenna rssi of last rx data frame/ {print $10}')
+            rsi_antenna3=$(echo "$wl_info" | awk '/per antenna rssi of last rx data frame/ {print $11}')
+            rsi_antenna4=$(echo "$wl_info" | awk '/per antenna rssi of last rx data frame/ {print $12}')
 
-            rsi_antenna1=$(wl -i $wlan sta_info $client | awk '/per antenna rssi of last rx data frame/ {print $9}')
-            rsi_antenna2=$(wl -i $wlan sta_info $client | awk '/per antenna rssi of last rx data frame/ {print $10}')
-            rsi_antenna3=$(wl -i $wlan sta_info $client | awk '/per antenna rssi of last rx data frame/ {print $11}')
-            rsi_antenna4=$(wl -i $wlan sta_info $client | awk '/per antenna rssi of last rx data frame/ {print $12}')
+            rsi_antenna_avg_1=$(echo "$wl_info" | awk '/per antenna average rssi of rx data frames/ {print $9}')
+            rsi_antenna_avg_2=$(echo "$wl_info" | awk '/per antenna average rssi of rx data frames/ {print $10}')
+            rsi_antenna_avg_3=$(echo "$wl_info" | awk '/per antenna average rssi of rx data frames/ {print $11}')
+            rsi_antenna_avg_4=$(echo "$wl_info" | awk '/per antenna average rssi of rx data frames/ {print $12}')
 
-            rsi_antenna_avg_1=$(wl -i $wlan sta_info $client | awk '/per antenna average rssi of rx data frames/ {print $9}')
-            rsi_antenna_avg_2=$(wl -i $wlan sta_info $client | awk '/per antenna average rssi of rx data frames/ {print $10}')
-            rsi_antenna_avg_3=$(wl -i $wlan sta_info $client | awk '/per antenna average rssi of rx data frames/ {print $11}')
-            rsi_antenna_avg_4=$(wl -i $wlan sta_info $client | awk '/per antenna average rssi of rx data frames/ {print $12}')
+            noise_antenna_1=$(echo "$wl_info" | awk '/per antenna noise floor/ {print $5}')
+            noise_antenna_2=$(echo "$wl_info" | awk '/per antenna noise floor/ {print $6}')
+            noise_antenna_3=$(echo "$wl_info" | awk '/per antenna noise floor/ {print $7}')
+            noise_antenna_4=$(echo "$wl_info" | awk '/per antenna noise floor/ {print $8}')
 
-            noise_antenna_1=$(wl -i $wlan sta_info $client | awk '/per antenna noise floor/ {print $5}')
-            noise_antenna_2=$(wl -i $wlan sta_info $client | awk '/per antenna noise floor/ {print $6}')
-            noise_antenna_3=$(wl -i $wlan sta_info $client | awk '/per antenna noise floor/ {print $7}')
-            noise_antenna_4=$(wl -i $wlan sta_info $client | awk '/per antenna noise floor/ {print $8}')
+            rx_rate_pkt=$(echo "$wl_info" | awk '/rate of last rx pkt:/ {print $6}')
+            tx1_rate_pkt=$(echo "$wl_info" | awk '/rate of last tx pkt:/ {print $6}')
+            tx2_rate_pkt=$(echo "$wl_info" | awk '/rate of last tx pkt:/ {print $9}')
 
-            rx_rate_pkt=$(wl -i $wlan sta_info $client | awk '/rate of last rx pkt:/ {print $6}')
-            tx1_rate_pkt=$(wl -i $wlan sta_info $client | awk '/rate of last tx pkt:/ {print $6}')
-            tx2_rate_pkt=$(wl -i $wlan sta_info $client | awk '/rate of last tx pkt:/ {print $9}')
+            link_bandwidth=$(echo "$wl_info" | awk '/link bandwidth/ {print $4}')
+            tx_failures=$(echo "$wl_info" | awk '/tx failures:/ {print $3}')
+            rx_decrypt_failures=$(echo "$wl_info" | awk '/rx decrypt failures:/ {print $4}')
 
-            link_bandwidth=$(wl -i $wlan sta_info $client | awk '/link bandwidth/ {print $4}')
-            tx_failures=$(wl -i $wlan sta_info $client | awk '/tx failures:/ {print $3}')
-            rx_decrypt_failures=$(wl -i $wlan sta_info $client | awk '/rx decrypt failures:/ {print $4}')
-
-            idle=$(wl -i $wlan sta_info $client | awk '/idle/ {print $2}')
-            online=$(wl -i $wlan sta_info $client | awk '/in network/ {print $3}')
+            idle=$(echo "$wl_info" | awk '/idle/ {print $2}')
+            online=$(echo "$wl_info" | awk '/in network/ {print $3}')
             rssi=$(wl -i $wlan rssi $client)
 
-            prefix="eth"
-            chann=$(echo "$wlan" | sed -e "s/^$prefix//")
+            if [ "$wlan" = "eth6" ]; then
+              chann="2"
+            elif [ "$wlan" = "eth7"  ]; then
+              chann="5"
+            fi            
 
-            columns="host=${ROUTER_MODEL},wifi=$wlan,client=$client,ip=$ip,hostname=$STATION_NAME,wifiBand=$chann"
-            points="tx2_rate_pkt=$tx2_rate_pkt,tx1_rate_pkt=$tx1_rate_pkt,rx_rate_pkt=$rx_rate_pkt,rssi=$rssi,online=$online,rx_rate_pkt=$rx_rate_pkt,rsi_antenna1=$rsi_antenna1,rsi_antenna2=$rsi_antenna2,rsi_antenna3=$rsi_antenna3,rsi_antenna4=$rsi_antenna4,rsi_antenna_avg_1=$rsi_antenna_avg_1,rsi_antenna_avg_2=$rsi_antenna_avg_2,rsi_antenna_avg_3=$rsi_antenna_avg_3,rsi_antenna_avg_4=$rsi_antenna_avg_4,noise_antenna_1=$noise_antenna_1,noise_antenna_2=$noise_antenna_2,noise_antenna_3=$noise_antenna_3,noise_antenna_4=$noise_antenna_4,wifiBand=$chann,link_bandwidth=$link_bandwidth,tx_failures=$tx_failures,rx_decrypt_failures=$rx_decrypt_failures"
+            columns="host=${ROUTER_MODEL},client=$client,ip=$ip,hostname=$STATION_NAME,wifiBand=$chann"
+            points="tx2_rate_pkt=$tx2_rate_pkt,tx1_rate_pkt=$tx1_rate_pkt,rx_rate_pkt=$rx_rate_pkt,rssi=$rssi,online=$online,rx_rate_pkt=$rx_rate_pkt,rsi_antenna1=$rsi_antenna1,rsi_antenna2=$rsi_antenna2,rsi_antenna3=$rsi_antenna3,rsi_antenna4=$rsi_antenna4,rsi_antenna_avg_1=$rsi_antenna_avg_1,rsi_antenna_avg_2=$rsi_antenna_avg_2,rsi_antenna_avg_3=$rsi_antenna_avg_3,rsi_antenna_avg_4=$rsi_antenna_avg_4,noise_antenna_1=$noise_antenna_1,noise_antenna_2=$noise_antenna_2,noise_antenna_3=$noise_antenna_3,noise_antenna_4=$noise_antenna_4,wifiBand=$chann,link_bandwidth=$link_bandwidth,tx_failures=$tx_failures,rx_decrypt_failures=$rx_decrypt_failures,wifiBand=$chann"
             
             #echo $columns
             #echo $points
@@ -124,9 +132,6 @@ mod_wifi_clients()
             echo $data >> $DATA_TEMP_FILE
             echo $data
             
-            #curl -is -XPOST "https://${DBURL}/write?db=${DBNAME}&u=${USER}&p=${PASS}" --data-binary "$data"  > /dev/null
-                #Print_Output "$SCRIPT_debug" "$data" "$WARN"
-            #$dir/export.sh "$data" "$SCRIPT_debug"
         done;
   done;
   
@@ -138,3 +143,5 @@ rm -f $DATA_TEMP_FILE
 lock
 mod_wifi_clients
 unlock
+
+#/opt/bin/bash $dir/mod_meshinfo.sh
